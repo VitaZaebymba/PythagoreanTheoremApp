a875d60f3201ac31a6de4a98bc94908823e98971
Vita <malavita29@mail.ru>
2022-06-30 17:22:27 +0300

    Init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,39 +5,10 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("My Log", "OnCreate")
     }
 
-    override fun onStart() {
-        super.onStart()
-        Log.d("My Log", "OnStart")
-    }
-
-    override fun onResume() {
-        super.onResume()
-        Log.d("My Log", "OnResume")
-    }
-
-    override fun onPause() {
-        super.onPause()
-        Log.d("My Log", "OnPause")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Log.d("My Log", "OnDestroy")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        Log.d("My Log", "OnStop")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        Log.d("My Log", "OnRestart")
-    }
 }
