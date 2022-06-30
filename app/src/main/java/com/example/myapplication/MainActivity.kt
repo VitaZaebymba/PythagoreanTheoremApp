c408f8f8390a0375295c3468400f9f1ecbb41a78
Vita <malavita29@mail.ru>
2022-06-30 19:34:14 +0300

    TestButton

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
+    }
+    fun onClickTest(view: View) {
+        val TextTest = findViewById<TextView>(R.id.TextTest)
+        TextTest.text = "Вита учит Котлин"
+
+    }
 }
