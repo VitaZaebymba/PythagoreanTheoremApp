0c9a468d3540a84ac8a330b0dd979d9824f6a38a
Vita <malavita29@mail.ru>
2022-08-01 15:47:44 +0300

    Region

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -14,49 +14,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
+        
 
-        bindingClass.button.setOnClickListener {
-            val result = bindingClass.edValue.text.toString()
-            bindingClass.imageView.visibility = View.VISIBLE
 
-            when (result) {
-
-                Const.MANAGER -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Получите ${Const.MANAGER_SUELDO}"
-                    if(bindingClass.pswd.text.toString() == Const.MANAGER_PASSWORD){
-                    bindingClass.tvResult.text = tempText
-                    bindingClass.imageView.setImageResource(R.drawable.viktor)
-                    }
-                    else  bindingClass.tvResult.text = "Неверный код"
-                }
-
-                Const.ASSISTENT -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Получите ${Const.ASSISTENT_SUELDO}"
-                    if(bindingClass.pswd.text.toString() == Const.ASSISTENT_PASSWORD){
-                        bindingClass.tvResult.text = tempText
-                        bindingClass.imageView.setImageResource(R.drawable.petr)
-                    }
-                    else  bindingClass.tvResult.text = "Неверный код"
-                }
-
-                Const.DIRECTOR -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    val tempText = "Получите ${Const.DIRECTOR_SUELDO}"
-                    if(bindingClass.pswd.text.toString() == Const.DIRECTOR_PASSWORD){
-                        bindingClass.tvResult.text = tempText
-                        bindingClass.imageView.setImageResource(R.drawable.nikolay)
-                    }
-                    else  bindingClass.tvResult.text = "Неверный код"
-                }
-
-                else -> {
-                    bindingClass.tvResult.visibility = View.VISIBLE
-                    bindingClass.tvResult.text = "Ошибка"
-                }
-            }
-        }
 
     }
 
