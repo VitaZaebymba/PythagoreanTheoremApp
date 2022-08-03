48135231961138fc57073c09ca43ec4d56231c46
Vita <malavita29@mail.ru>
2022-08-03 15:26:52 +0300

    init

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,18 +4,27 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.example.myapplication.constance.Const
 import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    lateinit var bindingClass: ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bindingClass = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(bindingClass.root)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
 
+    fun onClickResult(view: View){
 
+    }
+
+    private fun isFieldEmpty(): Boolean {
+        binding.apply {
+            if (edA.text.isNullOrEmpty()) edA.error = "Поле должно быть заполнено"
+            if (edB.text.isNullOrEmpty()) edB.error = "Поле должно быть заполнено"
+            return  edA.text.isNullOrEmpty() || edB.text.isNullOrEmpty()
+        }
 
 
     }
